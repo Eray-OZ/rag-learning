4b2a845db2dd3edf67535a8af75dd58eec9cad59
Eray <eray.py@gmail.com>
2025-06-28 16:57:08 +0300

    Data and RAG test

--- huggingface/sentence-transformer.py
+++ huggingface/sentence-transformer.py
@@ -2,13 +2,10 @@ from dotenv import load_dotenv
 import os
 from huggingface_hub import InferenceClient
 
-
 load_dotenv()
 
 token = os.getenv("HUGGING_FACE_TOKEN")
 
-
-    
 client = InferenceClient(
     provider="hf-inference",
     api_key= token,
@@ -16,15 +13,13 @@ client = InferenceClient(
 
 
 def get_embedding(text):
-    
     result = client.feature_extraction(
     text,
     model="sentence-transformers/all-MiniLM-L6-v2",
 )
-    
-    
     return result
 
-embedding = get_embedding("This is an example sentece")
 
+
+embedding = get_embedding("This is an example sentece")
 print(embedding)  # 384 boyutlu vektör
